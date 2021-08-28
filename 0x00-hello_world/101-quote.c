b9795317ea9d4431e8475f7a1c78fc94251ca3fa
lkiunga <linet.kiunga@strathmore.edu>
2021-08-28 10:37:09 -0700

    All tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *main - stderr
+ *
+ *Return: 1 upon success
+ */
+int main(void)
+{
+echo "and that piece of art is useful\" \- Dora Korpar, 2015-10-19", 1 >&2;
+return (1);
+}
