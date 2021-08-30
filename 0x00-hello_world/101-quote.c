2379a594f9fadf7a10680ae856a5974f302e232a
lkiunga <linet.kiunga@strathmore.edu>
2021-08-30 04:48:43 -0700

    Write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include<unistd.h>
 /**
  *main - stderr
  *
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-echo "and that piece of art is useful\" \- Dora Korpar, 2015-10-19", 1 >&2;
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 return (1);
 }
