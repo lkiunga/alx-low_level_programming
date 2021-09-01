207ea02479617d7205fabeb19ad4e387f7007818
lkiunga <linet.kiunga@strathmore.edu>
2021-09-01 05:34:43 -0700

    betty coding style

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
  *_isapha - print alpha
  *
  *@c : character to be traced
- *Description: return alpha letters
+ *Description : _isalpha lpha letters
  *Return: 1 true 0 false
  */
 int _isalpha(int c)
