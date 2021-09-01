e352e418b541e68c76454dd7cd2ef799a23cb76d
lkiunga <linet.kiunga@strathmore.edu>
2021-09-01 05:31:24 -0700

    betty

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- *main - print alpha
+ *_isapha - print alpha
  *
  *@c : character to be traced
  *Return: 1 true 0 false
  */
-int _isalpha (int c)
+int _isalpha(int c)
 {
-return((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')); 
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
