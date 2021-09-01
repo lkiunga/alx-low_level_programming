aeccb8af136fa8047a9b6b7852bf64e1e79845ce
lkiunga <linet.kiunga@strathmore.edu>
2021-09-01 05:39:50 -0700

    fix betty

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,6 +1,14 @@
+#include "main.h"
+/**
+ *_abs -entry point
+ *@n : no to be traced
+ *
+ *Description - returns the absolute value of numebrs
+ *Return:  n
+ */
 int _abs(int n)
 {
 if (n < 0)
 n = n * (-1);
-return n;
+return (n);
 }
