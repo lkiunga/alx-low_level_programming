655c04067398e3fdc46b64949d4f27b5ed4744f0
lkiunga <linet.kiunga@strathmore.edu>
2021-09-21 00:20:33 -0700

    Add task 01

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,4 @@
+Debugging
+1.What is debugging
+2.What are some methods of debugging manually
+3.How to read the error messages
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <limits.h>
+/* more headers goes there */
+
+/**                                                                        
+ *main - +is better than anything else                                     
+ *                                                                         
+ *Return: 0 on success always                                              
+*/
+int main(void)
+{
+int n = 0;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is Zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
+return (0);
+}
