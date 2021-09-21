389354e41a0d078751a45f4a2c6503cdef66c1ca
lkiunga <linet.kiunga@strathmore.edu>
2021-09-21 00:27:24 -0700

    Fix betty

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-        int i;
+int i;
 
-        i = 0;
-        positive_or_negative(i);
-	printf("results of the funtion\n");
-        return (0);
+i = 0;
+positive_or_negative(i);
+printf("results of the funtion\n");
+return (0);
 }
