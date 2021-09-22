efb598022b27e111889d4d22779de03fcd69c789
lkiunga <linet.kiunga@strathmore.edu>
2021-09-22 07:10:12 -0700

    remove addnnitional new line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 *print_diagonal - entry point
 *
@@ -10,18 +9,15 @@ void print_diagonal(int n)
 {
 int i, b;
 if (n <= 0)
-{
 _putchar('\n');
-}
 for (i = 0; i < n; i++)
 {
 for (b = 0; b < i; b++)
 {
 _putchar(' ');
 }
-_putchar(92);
+_putchar('\\');
 _putchar('\n');
 }
-_putchar('\n');
 return;
 }
