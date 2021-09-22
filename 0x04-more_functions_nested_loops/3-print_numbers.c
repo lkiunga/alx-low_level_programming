b04ff3306bc47744f9ce2a2134c7604493e3adb8
lkiunga <linet.kiunga@strathmore.edu>
2021-09-21 23:05:48 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_numbers - entry point
+ *
+ *Return:void
+ */
+void print_numbers(void)
+{
+char i;
+for (i='0'; i<='9'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+return;
+}
