f53f74bde67a8e9c58a5e4a20b61a0d6605e141f
lkiunga <linet.kiunga@strathmore.edu>
2021-09-21 22:25:42 -0700

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *_isupper - check for uppercase letters
+ *
+ *@c: the character to be traced
+ *Description: checks for Uppercase letters
+ *Return: 0 if false $ 1 if true
+ */
+int _isdigit(int c)
+{
+return (c >= 0 && c <= 9);
+}
