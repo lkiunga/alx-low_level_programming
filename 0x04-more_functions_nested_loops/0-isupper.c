9221a5b00bbdebc04df419ccf14cc94791c5188d
lkiunga <linet.kiunga@strathmore.edu>
2021-09-21 22:02:44 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *_isupper - check for uppercase letters
+ *
+ *@n: the character to be traced
+ *Description: checks for Uppercase letters
+ *Return: 0 if false $ 1 if true
+ */
+int _isuppper(int n)
+{
+return (n == 'A' && n <= 'Z');
+}
