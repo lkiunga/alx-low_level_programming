98904e49ba223600840b3c4d641bf9f384fe6dba
lkiunga <linet.kiunga@strathmore.edu>
2021-09-21 22:48:56 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *mul - check for uppercase letters
+ *
+ *@a: 1st integer for multiplication
+ *@b: 2nd integer for multiplication
+ *Description: Multiplies two integers
+ *Return: result of the multiplication
+ */
+int mul(int a, int b)
+{
+int result;
+result = a * b;
+return result;
+}
