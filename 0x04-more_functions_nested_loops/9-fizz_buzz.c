e935671ea613811d6b82633684f77fc291f08631
lkiunga <linet.kiunga@strathmore.edu>
2021-09-22 07:31:04 -0700

    changes

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,23 +15,16 @@ int i;
 for (i = 1; i <= 100; i++)
 {
 if (i % 15 == 0)
-{
 printf("FizzBuzz");
-}
-else if ((i % 3) == 0)
-{  
+else if (i % 3 == 0)
 printf("Fizz");
-}
-else if ((i % 5) == 0)
-{
+else if (i % 5 == 0)
 printf("Buzz");
-}
-else 
-{
+else
 printf("%i", i);
+if (i < 100)
+printf(" ");
 }
-printf(' ');
-}
-putchar('\n');
+printf("\n");
 return (0);
 }
